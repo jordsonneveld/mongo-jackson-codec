feeff75138fbcfa8b0e708e905d8be73936144e5
Jord Sonneveld <jord@sonneveld.org>
2017-10-28 14:09:54 -0700

    Make test compatible with JDK9's more accurate time instant.

--- src/test/java/fr/javatic/mongo/jacksonCodec/JacksonCodecTest.java
+++ src/test/java/fr/javatic/mongo/jacksonCodec/JacksonCodecTest.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2015 Jeff Yemin
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package fr.javatic.mongo.jacksonCodec;
@@ -30,53 +28,57 @@ import static org.junit.Assert.assertEquals;
 
 public class JacksonCodecTest {
 
-    private final JacksonCodec<BlogPost> codec = new JacksonCodec<>(
-            ObjectMapperFactory.createObjectMapper().registerModule(new JavaTimeBsonModule()),
-            CodecRegistries.fromProviders(new BsonValueCodecProvider()),
-            BlogPost.class);
+  private final JacksonCodec<BlogPost> codec = new JacksonCodec<>(
+      ObjectMapperFactory.createObjectMapper().registerModule(new JavaTimeBsonModule()),
+      CodecRegistries.fromProviders(new BsonValueCodecProvider()),
+      BlogPost.class);
 
-    private final OffsetDateTime testOffsetDateTime = OffsetDateTime.now();
+  private final OffsetDateTime testOffsetDateTime = OffsetDateTime.now();
 
-    private static BsonDocument createBlogPostBson(String id, OffsetDateTime offsetDateTime){
-        BsonDocument expectedDocument = new BsonDocument();
-        expectedDocument.put("offsetDateTime", new BsonDateTime(offsetDateTime.toInstant().toEpochMilli()));
-        expectedDocument.put("_id", new BsonString(id));
+  private static BsonDocument createBlogPostBson(String id, OffsetDateTime offsetDateTime) {
+    BsonDocument expectedDocument = new BsonDocument();
+    expectedDocument.put("offsetDateTime",
+        new BsonDateTime(offsetDateTime.toInstant().toEpochMilli()));
+    expectedDocument.put("_id", new BsonString(id));
 
-        return expectedDocument;
-    }
+    return expectedDocument;
+  }
 
-    @Test
-    public void testEncoderClass() {
-        codec.getEncoderClass().equals(BlogPost.class);
-    }
+  @Test
+  public void testEncoderClass() {
+    codec.getEncoderClass().equals(BlogPost.class);
+  }
 
-    @Test
-    public void testEncoding() {
-        BsonDocumentWriter writer = new BsonDocumentWriter(new BsonDocument());
+  @Test
+  public void testEncoding() {
+    BsonDocumentWriter writer = new BsonDocumentWriter(new BsonDocument());
 
-        BlogPost blogPost = new BlogPost();
-        blogPost.setId("/first_blog");
-        blogPost.setOffsetDateTime(testOffsetDateTime);
+    BlogPost blogPost = new BlogPost();
+    blogPost.setId("/first_blog");
+    blogPost.setOffsetDateTime(testOffsetDateTime);
 
-        codec.encode(writer, blogPost, EncoderContext.builder().build());
+    codec.encode(writer, blogPost, EncoderContext.builder().build());
 
-        assertEquals(createBlogPostBson(blogPost.getId(), blogPost.getOffsetDateTime()), writer.getDocument());
-    }
+    assertEquals(createBlogPostBson(blogPost.getId(), blogPost.getOffsetDateTime()),
+        writer.getDocument());
+  }
 
-    @Test
-    public void testDecoding() {
-        String idValue = "/first_blog";
-        BsonDocumentReader reader = new BsonDocumentReader(createBlogPostBson(idValue, testOffsetDateTime));
+  @Test
+  public void testDecoding() {
+    String idValue = "/first_blog";
+    BsonDocumentReader reader =
+        new BsonDocumentReader(createBlogPostBson(idValue, testOffsetDateTime));
 
-        BlogPost blogPost = codec.decode(reader, DecoderContext.builder().build());
+    BlogPost blogPost = codec.decode(reader, DecoderContext.builder().build());
 
-        BlogPost expectedBlogPost = new BlogPost();
-        expectedBlogPost.setId(idValue);
-        expectedBlogPost.setOffsetDateTime(testOffsetDateTime);
+    BlogPost expectedBlogPost = new BlogPost();
+    expectedBlogPost.setId(idValue);
+    expectedBlogPost.setOffsetDateTime(testOffsetDateTime);
 
-        assertEquals(expectedBlogPost, blogPost);
-        assertEquals(testOffsetDateTime.toInstant(), blogPost.getOffsetDateTime().toInstant());
-    }
+    assertEquals(expectedBlogPost, blogPost);
+    assertEquals(testOffsetDateTime.toInstant().toEpochMilli(),
+        blogPost.getOffsetDateTime().toInstant().toEpochMilli());
+  }
 
 
 }
